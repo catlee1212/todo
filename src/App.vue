8a14d25025d20c63aef0cc6ac85b555d69656255
Marlene Wiesmair <marlene.wiesmair@triceonic.com>
2023-11-29 12:36:02 +0100

    Setting up project.

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <TodoComponent />
+</template>
+
+<script>
+import TodoComponent from "./components/TodoComponent.vue";
+
+export default {
+  name: "App",
+  components: {
+    TodoComponent,
+  },
+};
+</script>
